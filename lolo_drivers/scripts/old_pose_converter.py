6ee1df8f35b5fa1172a515956bef56014610bdaa
Nils Bore <nilsbore@gmail.com>
2020-12-07 20:59:35 +0100

    Fixed a couple of issues

--- lolo_drivers/scripts/old_pose_converter.py
+++ lolo_drivers/scripts/old_pose_converter.py
@@ -5,36 +5,50 @@ from smarc_msgs.msg import LatLonOdometry, LatLonStamped, FloatStamped
 from smarc_msgs.srv import LatLonToUTMOdometry
 from geometry_msgs.msg import QuaternionStamped
 from nav_msgs.msg import Odometry
+import math
 
-quat = None
-altitude = 0.
-pub = None
+class OldPoseConverter(object):
 
-def quat_callback(msg):
-    quat = msg.quaternion
+    def __init__(self):
+        self.quat = None
+        self.altitude = 0.
+        self.pub = rospy.Publisher('dr/odom', Odometry, queue_size=10)
+        self.quat_sub = rospy.Subscriber("core/state/orientation", QuaternionStamped, self.quat_callback)
+        self.depth_sub = rospy.Subscriber("core/state/depth", FloatStamped, self.depth_callback)
+        self.lat_lon_sub = rospy.Subscriber("core/state/position", LatLonStamped, self.lat_lon_callback)
 
-def depth_callback(msg):
-    altitude = -msg.data
+    def quat_callback(self, msg):
+        #print("Quat callback!")
+        self.quat = msg.quaternion
 
-def lat_lon_callback(msg):
-    odom = LatLonOdometry()
-    odom.lat_lon_pose.position.latitude = msg.latitude
-    odom.lat_lon_pose.position.longitude = msg.longitude
-    odom.lat_lon_pose.position.altitude = altitude
-    if quat is not None:
-        odom.lat_lon_pose.orientation = quat
-    try:
-        translate_odom = rospy.ServiceProxy('lat_lon_to_utm_odom', LatLonToUTMOdometry)
-        resp = translate_odom(odom)
-    except rospy.ServiceException as e:
-        print("Service call failed: %s"%e)
-        return
-    pub.publish(resp.odom)
+    def depth_callback(self, msg):
+        self.altitude = -msg.data
+
+    def lat_lon_callback(self, msg):
+        odom = LatLonOdometry()
+        odom.lat_lon_pose.position.latitude = 180./math.pi*msg.latitude
+        odom.lat_lon_pose.position.longitude = 180./math.pi*msg.longitude
+        odom.lat_lon_pose.position.altitude = self.altitude
+
+        print "Got ", msg.latitude, msg.longitude, self.altitude
+
+        #print "Quat in lat lon: ", self.quat
+        if self.quat is not None:
+            odom.lat_lon_pose.orientation = self.quat
+            #print "Adding odom: ", self.quat
+        else:
+            odom.lat_lon_pose.orientation.w = 1.
+
+        try:
+            translate_odom = rospy.ServiceProxy('lat_lon_to_utm_odom', LatLonToUTMOdometry)
+            resp = translate_odom(odom)
+        except rospy.ServiceException as e:
+            print("Service call failed: %s"%e)
+            return
+        print "Got ", resp.odom.pose.pose.position
+        self.pub.publish(resp.odom)
 
 if __name__ == '__main__':
     rospy.init_node('old_pose_converter')
-    pub = rospy.Publisher('dr/odom', Odometry, queue_size=10)
-    quat_sub = rospy.Subscriber("core/state/orientation", QuaternionStamped, quat_callback)
-    depth_sub = rospy.Subscriber("core/state/depth", FloatStamped, depth_callback)
-    lat_lon_sub = rospy.Subscriber("core/state/position", LatLonStamped, lat_lon_callback)
+    conv = OldPoseConverter()
     rospy.spin()
